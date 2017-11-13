f89c7f1df2b150227e8b45d95a7169434126dd1f
ValimarTheAshenKnight <david.f.buetikofer@gmail.com>
2017-11-13 16:15:48 +0100

    App angefangen
    Den Titel Bildschirm fertig gebracht und die Menus angefangen

---
+++ app/src/main/java/com/example/dbuetikofer/dasherguy/SelectScreen.java
@@ -0,0 +1,23 @@
+package com.example.dbuetikofer.dasherguy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SelectScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_screen);
+
+    }
+
+
+    public void optionsOpen(View view){
+        Intent intent = new Intent(SelectScreen.this, Options.class);
+        startActivity(intent);
+
+    }
+}
--- app/src/main/java/com/example/dbuetikofer/dasherguy/StartScreen.java
+++ app/src/main/java/com/example/dbuetikofer/dasherguy/StartScreen.java
@@ -1,13 +1,29 @@
 package com.example.dbuetikofer.dasherguy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class StartScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
     }
+
+    public void screenTapped(View view) {
+        Intent i = new Intent(StartScreen.this, SelectScreen.class);
+        startActivity(i);
+    }
+
 }
